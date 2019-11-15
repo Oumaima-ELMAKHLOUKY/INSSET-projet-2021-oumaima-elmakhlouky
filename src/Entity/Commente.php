8a312574087e5e8420860f17e5d1b6e6e96ac0a2
VC-Semih <55637588+VC-Semih@users.noreply.github.com>
2019-11-15 14:31:04 +0100

    Entites

---
+++ src/Entity/Commente.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Commente",
+ *     uniqueConstraints={
+ *     @ORM\UniqueConstraint(name="idCom", columns={"user_id", "article_id"})
+ * })
+ * */
+class commente
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $date;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $text;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="Commentes")
+     */
+    protected $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Article")
+     */
+    protected $article;
+
+    public function __toString()
+    {
+        $format = "Article (Id: %s, %s, %s)\n";
+        return sprintf($format, $this->id, $this->user, $this->article);
+    }
+
+}
